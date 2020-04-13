1407c2e67df705ddf8375a21217b60a76d061fd5
Merrii <merrii@bk.ru>
2020-04-13 17:17:41 +0200

    Making text editable

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,9 +1,12 @@
+import '../../helpers/iframeLoader.js'
 import axios from 'axios';
 import React, { Component } from 'react';
 
 export default class Editor extends Component {
     constructor() {
         super();
+
+        this.currentPage = 'index.html'
 
         this.state = {
             pageList: [],
@@ -13,7 +16,7 @@ export default class Editor extends Component {
         this.createNewPage = this.createNewPage.bind(this);
     }
     componentDidMount() {
-        this.loadPageList();
+        this.init(this.currentPage);
     }
 
     loadPageList() {
@@ -39,23 +42,55 @@ export default class Editor extends Component {
             .catch(() => alert('The page is not exist'));
     }
 
+    init(page) {
+        this.iframe = document.querySelector('iframe');
+        this.open(page);
+        this.loadPageList();
+    }
+
+    open(page) {
+        this.currentPage = `../${page}`;
+        this.iframe.load(this.currentPage, () => {
+            const body = this.iframe.contentDocument.body;
+            let textNodes = [];
+            function recursy(element) {
+                element.childNodes.forEach(node => {
+                    if (node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
+                        textNodes.push(node);
+                    } else {
+                        recursy(node);
+                    }
+                });
+                
+            }
+            recursy(body);
+            textNodes.forEach(node => {
+                const wrapper = this.iframe.contentDocument.createElement('text-editor');
+                node.parentNode.replaceChild(wrapper, node);
+                wrapper.appendChild(node);
+                wrapper.contentEditable = 'true';
+            })
+        });       
+    }
+
     render(){
-        const {pageList} = this.state;
-        const pages = pageList.map((page,i )=> {
-            return (
-                <h1 key={i}>
-                    {page}
-                    <a href="#" onClick={() => this.deletePage(page)}>(x)</a>
-                </h1>
-            )
-        })
+        // const {pageList} = this.state;
+        // const pages = pageList.map((page,i )=> {
+        //     return (
+        //         <h1 key={i}>
+        //             {page}
+        //             <a href="#" onClick={() => this.deletePage(page)}>(x)</a>
+        //         </h1>
+        //     )
+        // })
         return (
-            <>
-                <input onChange={(e) => {this.setState({newPageName: e.target.value})}} 
-                type="text" />
-                <button onClick={this.createNewPage}>Create page</button>
-                {pages}
-            </>
+            <iframe src={this.currentPage} frameBorder="0"></iframe>
+            // <>
+            //     <input onChange={(e) => {this.setState({newPageName: e.target.value})}} 
+            //     type="text" />
+            //     <button onClick={this.createNewPage}>Create page</button>
+            //     {pages}
+            // </>
         )
 
     }
